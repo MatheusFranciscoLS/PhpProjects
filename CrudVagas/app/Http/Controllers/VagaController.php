267cdcfd850e6d428a1e957ac470917115e1af9a
MatheusFranciscoLS <matheusf.ls@hotmail.com>
2024-08-05 21:58:22 -0300

    05/08/24

---
+++ CrudVagas/app/Http/Controllers/VagaController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Vaga;
+use Illuminate\Http\Request;
+use Ramsey\Uuid\Type\Decimal;
+
+class VagaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $vagas = Vaga::all();
+        return view('vagas.index', compact('vagas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('vagas.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'titulo' => 'required',
+            'descricao' => 'required',
+            'setor' => 'required',
+            'remuneracao' => 'required|decimal',
+            'empresa' => 'required'
+        ]);
+        Vaga::create($request->all());
+        return redirect()->route('vagas.index')
+            ->with('success', 'Vaga criada com sucesso.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vaga $vaga)
+    {
+        return view('vagas.show', compact('vaga'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Vaga $vaga)
+    {
+        return view('vagas.edit', compact('vaga'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Vaga $vaga)
+    {
+        $request->validate([
+            'titulo' => 'required',
+            'descricao' => 'required',
+            'setor' => 'required',
+            'remuneracao' => 'required|decimal',
+            'empresa' => 'required'
+        ]);
+        Vaga::update($request->all());
+        return redirect()->route('vagas.index')
+            ->with('success', 'Vaga atualizada com sucesso.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vaga $vaga)
+    {
+        $vaga->delete();
+        return redirect()->route('vagas.index')
+            ->with('success', 'Vaga Deletada com Sucesso.');
+    }
+}
